521ca479a52c4c81c90269113e10ddf1ea770d98
Virufighter <alvinpoulose4@gmail.com>
2022-11-25 18:02:27 +0530

    friend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddFriendComponent } from './add-friend/add-friend.component';
+import { ViewFriendComponent } from './view-friend/view-friend.component';
+import { SerachFriendComponent } from './serach-friend/serach-friend.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+const myrout:Routes=[
+  {
+    path:"",
+    component:AddFriendComponent
+  },{
+    path:"view",
+    component:ViewFriendComponent
+    
+  },{
+    path:"search",
+    component:SerachFriendComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddFriendComponent,
+    ViewFriendComponent,
+    SerachFriendComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myrout)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/serach-friend/serach-friend.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-serach-friend',
+  templateUrl: './serach-friend.component.html',
+  styleUrls: ['./serach-friend.component.css']
+})
+export class SerachFriendComponent {
+
+}
